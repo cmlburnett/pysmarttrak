91c649a447477cbc351ce4abaa1797ea49b63e53
Colin ML Burnett <cmlburnett@gmail.com>
2023-10-13 11:55:33 -0500

    Also supports PolyScience CA10 chiller

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # pysmarttrak
 python script to interact with a Sierra Instruments Smart Trak 50 mass flow controller
 
-	with SmartTrak50('/dev/ttyUSB0') as s:
+	with SmartTrak50.open('/dev/ttyUSB0') as s:
 		print(s.GetFlow())
 
 Usage is pretty straight forward, see function names for available queries.
 
 https://www.sierrainstruments.com/products/50series.html
+
+
+Also added is the PolyScience CA10 refrigerating recirculating chiller.
+
+	with PolyScienceCA10.open('/dev/ttyUSB0') as s:
+		print(s.GetSetPoint())
+
+Usage is pretty straight forward, see function names for available queries.
+
+https://www.digivac.com/product/polyscience-durachill-ca10-chiller-10c-to-70c/
 
--- readflow.py
+++ readflow.py
@@ -175,9 +175,260 @@ class SmartTrak50:
 		gass = ret.split('Gass')[1]
 		return float(gass)
 
-if __name__ == '__main__':
-	# Test use by querying for all the things
+class PolyScienceCA10:
+	"""
+	Uses python3-serial to communicate with a PolyScience CA10 egirgerated recirculating chiller.
 
+	with PolyScienceCA10('/dev/tty...') as s: s.GetSetPoint()
+
+	An example usage above.
+	"""
+
+	def __init__(self, port):
+		self._port = port
+		self._serial = None
+
+	@property
+	def Port(self): return self._port
+
+	def _open(self):
+		if not self._serial:
+			self._serial = serial.Serial(self._port, 9600, timeout=5)
+
+	def _close(self):
+		self._serial.close()
+		self._serial = None
+
+	@contextlib.contextmanager
+	def open(port):
+		s = None
+		try:
+			s = PolyScienceCA10(port)
+			s._open()
+			yield s
+		finally:
+			if s:
+				s._close()
+
+	def write(self, msg):
+		"""Encode into ASCII and write over serial"""
+		self._serial.write(msg.encode('ascii'))
+
+	def readline(self):
+		"""Read a line and decode from ASCII, check the LRC, and return the message"""
+		# Reading is painfully slow (seconds for a response), not sure why
+		# It is CR based not LF or CRLF, so it's not a timeout problem
+		r = self._serial.read_until('\r')
+		z = r.decode('ascii').strip('\r')
+
+		return z
+
+	# --------------------------------------------------------
+	# Functions
+
+	def PowerOn(self):
+		self.write('SO1\r')
+		ret = self.readline()
+		if ret == '!':
+			return
+		else:
+			raise ValueError("Failed to power on")
+
+	def PowerOff(self):
+		self.write('SO0\r')
+		ret = self.readline()
+		if ret == '!':
+			return
+		else:
+			raise ValueError("Failed to power off")
+
+	def PowerStatus(self):
+		self.write('RW\r')
+		ret = self.readline()
+		return ret[0] == '1'
+
+	def GetSetPoint(self):
+		self.write('RS\r')
+		ret = self.readline()
+		return float(ret)
+
+	def SetSetPoint(self, t):
+		self.write('SS%.1f\r' % t)
+		ret = self.readline()
+		if ret == '!':
+			return
+		else:
+			raise ValueError("Failed to set the set point to %.1f" % t)
+
+	def GetTemperature(self):
+		"""Read temperature in whatever units is set"""
+		self.write('RT\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetTemperatureUnits(self):
+		"""Read temperature units: 'C' == celcius, 'F' == Fahrenheit"""
+		self.write('RU\r')
+		ret = self.readline()
+		return ret[0]
+
+	def GetPressurePSI(self):
+		"""Read in pounds per square inch (PSI)"""
+		self.write('RP\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetPressureKPA(self):
+		"""Read in kilopascals (kPa)"""
+		self.write('RK\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetFlowGPM(self):
+		"""Gallons per minute"""
+		self.write('RG\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetFlowLPM(self):
+		"""Liters per minute"""
+		self.write('RL\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetTotalPower(self):
+		v = self.GetPowerVoltage()
+		a1 = self.GetCompressorAmps()
+		a2 = self.GetPumpAmps()
+
+		return v*(a1+a2)
+
+	def GetPowerVoltage(self):
+		self.write('RV\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetCompressorAmps(self):
+		self.write('RCA\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetPumpAmps(self):
+		self.write('RPA\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetFluidLevelStatus(self):
+		"""Get fluid leve status: True == good, Fale == low"""
+		self.write('RX\r')
+		ret = self.readline()
+		# Good is 0, low is 1
+		return ret[0] == '0'
+
+	def GetFluidLevel(self):
+		self.write('RFL\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetAmbientTemperature(self):
+		"""Get ambient tempreature in whatever units is set (see GetTemperatureUnits())"""
+		self.write('RA\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetAmbientHumidity(self):
+		"""Get ambient relative humidity in percent"""
+		self.write('RRH\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetAmbientPressure(self):
+		"""Get ambient barometric pressure, I think this is in hectopascals (hPa)"""
+		self.write('RBP\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetOperatingHours(self):
+		"""Get operating hours"""
+		self.write('ROC\r')
+		ret = self.readline()
+		return float(ret)
+
+	def GetOperatingCycles(self):
+		"""Get on/off cycles"""
+		self.write('RCC\r')
+		ret = self.readline()
+		return int(ret)
+
+	def GetSystemFault(self):
+		"""Get system fault (0 == system ok)"""
+		self.write('RF\r')
+		ret = self.readline()
+		return int(ret)
+
+def TestPolyScienceCA10():
+	with PolyScienceCA10.open('/dev/ttyUSB0') as s:
+		ret = s.PowerStatus()
+		if ret:
+			print('Power is On')
+		else:
+			print('Power is Off')
+
+		f = s.GetSystemFault()
+		print('System Fault %d' % f)
+
+		f = s.GetPowerVoltage()
+		print('Power voltage is %.1f volts' % f)
+
+		f = s.GetCompressorAmps()
+		print('Compressor Amps %.1f Amps' % f)
+
+		f = s.GetPumpAmps()
+		print('Pump Amps %.1f Amps' % f)
+
+		f = s.GetTotalPower()
+		print('Power Consumption %.1f Watts' % f)
+
+
+		f = s.GetOperatingHours()
+		print('Operating Hours %.1f hours' % f)
+
+		f = s.GetOperatingCycles()
+		print('Operating Hours %d cycles' % f)
+
+		ret = s.GetFluidLevelStatus()
+		if ret:
+			print('Fluid Level Good')
+		else:
+			print('Fluid Level Low')
+
+		f = s.GetFluidLevel()
+		print('Fluid Level %.1f' % f)
+
+
+		f = s.GetAmbientTemperature()
+		print('Ambient Temperatures %.1f' % f)
+
+		f = s.GetAmbientHumidity()
+		print('Ambient Humidity %.1f %%' % f)
+
+		f = s.GetAmbientPressure()
+		print('Ambient Pressure %.1f hPa' % f)
+
+
+		f = s.GetSetPoint()
+		print('Set point %.2f' % f)
+
+		f = s.GetTemperature()
+		print('Temp %.2f' % f)
+
+		f = s.GetPressurePSI()
+		print('Pressure %.2f PSI' % f)
+
+		f = s.GetFlowLPM()
+		print('Flow %.2f LPM' % f)
+
+def TestSmartTrak50():
 	with SmartTrak50.open('/dev/ttyUSB0') as s:
 		f = s.GetFlow()
 		print('Flow %.4f' % f)
@@ -208,4 +459,10 @@ if __name__ == '__main__':
 
 		f = s.GetSpan()
 		print('Span %.4f' % f)
+
+if __name__ == '__main__':
+	# Test use by querying for all the things
+
+	TestPolyScienceCA10()
+	#TestSmartTrak50()
 
